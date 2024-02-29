3f3c8e6e67af8eab953330e09499f15e66525664
Pato <dkno_c1@hotmail.com>
2024-02-29 00:23:36 -0300

    player movement with keys

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -15,14 +15,20 @@ public partial class Player : CharacterBody3D
     private float _jumpForce = 5f;
     private float _gravity = 9.8f;
     
-    // camera look
+    // Camera look
     private float _minLookAngle = -90f;
     private float _maxLookAngle = 90f;
     private float _sensitivity = 10f;
 
+    // Aux
     private Vector3 _vel = new Vector3();
     private Vector2 _mouseDelta = new Vector2();
+    private Vector2 _input = new Vector2();
+    private Vector3 _forward = new Vector3();
+    private Vector3 _right = new Vector3();
+    private Vector3 _relativeDir = new Vector3();
 
+    // Components
     private Camera3D _camera3D;
     private Node3D _muzzle;
 
@@ -34,5 +40,44 @@ public partial class Player : CharacterBody3D
         {
             GD.PushError("null");
         }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        _vel.X = 0;
+        _vel.Z = 0;
+        _input = new Vector2();
+
+        if (Input.IsActionPressed("ui_up"))
+            _input.Y -= 1;
+        if (Input.IsActionPressed("ui_down"))
+            _input.Y += 1;
+        if (Input.IsActionPressed("ui_left"))
+            _input.X -= 1;
+        if (Input.IsActionPressed("ui_right"))
+            _input.X += 1;
+
+        _input = _input.Normalized();
+        
+        // Get the forward and right directions
+        _forward = GlobalTransform.Basis.Z;
+        _right = GlobalTransform.Basis.X;
+        _relativeDir = (_forward * _input.Y + _right * _input.X);
+
+        // Set the velocity
+        _vel.X = _relativeDir.X * _moveSpeed;
+        _vel.Z = _relativeDir.Z * _moveSpeed;
+        
+        // Apply gravity
+        _vel.Y -= _gravity * (float)delta;
+        
+        // Move the player
+        Velocity = _vel;
+        MoveAndSlide();
+        
+        // Jumping
+        if (Input.IsActionPressed("ui_accept") && IsOnFloor())
+            _vel.Y = _jumpForce;
+        
     }
 }
