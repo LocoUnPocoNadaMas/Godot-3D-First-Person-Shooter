8c3ddce16f1cd788369f76c1f532e8b62e4da33e
Pato <dkno_c1@hotmail.com>
2024-03-02 17:21:12 -0300

    added ui script

---
+++ Assets/Scrips/Ui.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+namespace Godot3DFirstPersonShooter.Assets.Scrips;
+
+public partial class Ui : Control
+{
+    private TextureProgressBar _barHealth;
+    private Label _txtAmmo;
+    private Label _txtScore;
+
+    public override void _Ready()
+    {
+        _barHealth = GetNode<TextureProgressBar>("barHealth");
+        _txtAmmo = GetNode<Label>("txtAmmo");
+        _txtScore = GetNode<Label>("txtScore");
+        if (_barHealth == null || _txtAmmo == null || _txtScore == null)
+        {
+            GD.PushError("null");
+            GetTree().Quit();
+        }
+    }
+
+    private void BarHealthUpdate(int curHp, int maxHp)
+    {
+        _barHealth.MaxValue = maxHp;
+        _barHealth.Value = curHp;
+        
+    }
+
+    private void TxtAmmoUpdate(int ammo)
+    {
+        _txtAmmo.Text = "Ammo: " + ammo;
+    }
+
+    private void TxtScoreUpdate(int score)
+    {
+        _txtScore.Text = "Score: " + score;
+    }
+}
