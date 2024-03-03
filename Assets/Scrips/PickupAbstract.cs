d922a3f94717e58254fb5102acaf1ea1e0ac0875
Pato <dkno_c1@hotmail.com>
2024-03-02 22:26:40 -0300

    tweaking

--- Assets/Scrips/PickupAbstract.cs
+++ Assets/Scrips/PickupAbstract.cs
@@ -15,14 +15,36 @@ public partial class PickupAbstract : Area3D
     
     // bobling
     private float _startPosY;
+    
     [ExportCategory("Movement")]
     [Export] private float _bobHeight = 1f;
     [Export] private float _bobSpeed = 1f;
+    
     private bool _bobbingUp = true;
+    private Vector3 _position;
 
     public override void _Ready()
     {
         _startPosY = Position.Y;
+    }
+
+    public override void _Process(double delta)
+    {
+        // Move up and down
+        // translation.y += (bobSpeed if bobbingUp == true else -bobSpeed) * delta
+        _position = Position;
+        _position.Y += (_bobbingUp ? _bobSpeed : -_bobSpeed) * (float)delta;
+        Position = _position;
+        // if it at the top
+        if (_bobbingUp && _position.Y > _startPosY + _bobHeight)
+            _bobbingUp = false;
+        // if it at bottom
+        else
+        {
+            if (!_bobbingUp && _position.Y < _startPosY)
+                _bobbingUp = true;
+        }
+
     }
 
     private void _OnBodyEntered(Node3D body)
